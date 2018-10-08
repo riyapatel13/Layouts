e320225d9a7335649db47cf929bf510eb0892c62
Riya Patel <riaupatel13@yahoo.com>
2018-10-08 10:56:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a10016322/layouts/LayoutActivity.java
@@ -0,0 +1,13 @@
+package com.example.a10016322.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
